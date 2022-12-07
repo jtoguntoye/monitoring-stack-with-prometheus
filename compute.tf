d7b08bb4ad3938587ff7b0f3fa79cd4ea2eb1c12
Joel Oguntoye <joeloguntoye@gmail.com>
2022-12-07 08:54:01 +0100

    Created compute resources

---
+++ compute.tf
@@ -0,0 +1,64 @@
+data "aws_ami" "server_ami" {
+    most_recent = true
+
+    owners = ["099720109477"]
+
+    filter {
+      name = "name"
+      values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+    }
+}
+
+resource "random_id" "node_id" {
+  count = var.main_instance_count
+  byte_length = 2
+}
+
+
+
+resource "aws_instance" "mtg_main_instance" {
+  count = var.main_instance_count
+  ami           = data.aws_ami.server_ami.id
+  instance_type = var.main_instance_type
+  key_name = aws_key_pair.mtg_instance_key.id
+  user_data = templatefile("./main-userdata.tpl", {new_hostname = "mtg_main_instance-${random_id.node_id[count.index].dec}"})
+  vpc_security_group_ids = [aws_security_group.public_sg.id]
+  subnet_id = aws_subnet.mtg_public_subnet[count.index].id
+  root_block_device {
+    volume_size =   var.main_volume_size
+  }
+
+  tags = {
+    Name = "mtg_main_instance-${random_id.node_id[count.index].dec}"
+  }
+
+  provisioner "local-exec" {
+    command = "printf '\n${self.public_ip}' >> ./aws_hosts"
+    interpreter=["bash", "-c"]
+
+  }
+
+  provisioner "local-exec" {
+    when = destroy
+    command = "sed -i '/^[0-9]/d' ./aws_hosts"
+  }
+}
+
+resource "aws_key_pair" "mtg_instance_key" {
+ key_name = var.key_name 
+ public_key = file(var.public_key_path)
+}
+
+resource "null_resource" "grafana_update" {
+  count = var.main_instance_count
+  connection {
+    type = "ssh"
+    private_key = file("c:\\users\\user\\desktop\\devops\\mtgkey")
+    user = ubuntu
+    host = aws_instance.mtg_main_instance[count.index].public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = ["sudo apt upgrade -y grafana && touch upgrade.log && echo 'I updated Grafana aplication'>> update.log"]
+  }
+}
--- variable.tf
+++ variable.tf
@@ -7,3 +7,26 @@ variable "access_ip" {
   type = list(string)
   default = [ "0.0.0.0/0" ]
 }
+
+variable "main_instance_type" {
+    type = string
+    default = "t2.micro"
+}
+
+variable "main_volume_size" {
+    type = number
+    default = 8
+}
+
+variable "main_instance_count" {
+  type = number
+  default = 1
+}
+
+variable "key_name" {
+  type = string
+}
+
+ variable "public_key_path" {
+   type = string
+ }
